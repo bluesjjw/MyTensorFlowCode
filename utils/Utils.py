b6122fccc900ac088acd639afffdd26f8a38b932
Jiawei Jiang <blue.jwjiang@gmail.com>
2016-08-11 22:55:22 +0800

    read cifar10

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,5 +1,10 @@
+from __future__ import division, print_function, absolute_import
+
 import numpy as np
 import tensorflow as tf
+
+
+import matplotlib.pyplot as plt
 
 def xavier_init(fan_in, fan_out, constant = 1):
     low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
@@ -7,3 +12,107 @@ def xavier_init(fan_in, fan_out, constant = 1):
     return tf.random_uniform((fan_in, fan_out),
                              minval = low, maxval = high,
                              dtype = tf.float32)
+
+
+# =======================
+# TARGETS (LABELS) UTILS
+# =======================
+def to_categorical(y, nb_classes):
+    """ to_categorical.
+    Convert class vector (integers from 0 to nb_classes)
+    to binary class matrix, for use with categorical_crossentropy.
+    Arguments:
+        y: `array`. Class vector to convert.
+        nb_classes: `int`. Total number of classes.
+    """
+    y = np.asarray(y, dtype='int32')
+    if not nb_classes:
+        nb_classes = np.max(y)+1
+    Y = np.zeros((len(y), nb_classes))
+    for i in range(len(y)):
+        Y[i, y[i]] = 1.
+    return Y
+
+def plot_images(imgs, loc, title=None, channels=1):
+    '''Plot an array of images.
+    We assume that we are given a matrix of data whose shape is (n*n, s*s*c) --
+    that is, there are n^2 images along the first axis of the array, and each
+    image is c squares measuring s pixels on a side. Each row of the input will
+    be plotted as a sub-region within a single image array containing an n x n
+    grid of images.
+    '''
+    n = int(np.sqrt(len(imgs)))
+    assert n * n == len(imgs), 'images array must contain a square number of rows!'
+    s = int(np.sqrt(len(imgs[0]) / channels))
+    assert s * s == len(imgs[0]) / channels, 'images must be square!'
+
+    img = np.zeros(((s+1) * n - 1, (s+1) * n - 1, channels), dtype=imgs[0].dtype)
+    for i, pix in enumerate(imgs):
+        r, c = divmod(i, n)
+        img[r * (s+1):(r+1) * (s+1) - 1,
+            c * (s+1):(c+1) * (s+1) - 1] = pix.reshape((s, s, channels))
+
+    img -= img.min()
+    img /= img.max()
+
+    ax = plt.gcf().add_subplot(loc)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    ax.set_frame_on(False)
+    ax.imshow(img.squeeze(), cmap=plt.cm.gray)
+    if title:
+        ax.set_title(title)
+
+
+def plot_layers(weights, tied_weights=False, channels=1):
+    '''Create a plot of weights, visualized as "bottom-level" pixel arrays.'''
+    if hasattr(weights[0], 'get_value'):
+        weights = [w.get_value() for w in weights]
+    k = min(len(weights), 9)
+    imgs = np.eye(weights[0].shape[0])
+    for i, weight in enumerate(weights[:-1]):
+        imgs = np.dot(weight.T, imgs)
+        plot_images(imgs,
+                    100 + 10 * k + i + 1,
+                    channels=channels,
+                    title='Layer {}'.format(i+1))
+    weight = weights[-1]
+    n = weight.shape[1] / channels
+    if int(np.sqrt(n)) ** 2 != n:
+        return
+    if tied_weights:
+        imgs = np.dot(weight.T, imgs)
+        plot_images(imgs,
+                    100 + 10 * k + k,
+                    channels=channels,
+                    title='Layer {}'.format(k))
+    else:
+        plot_images(weight,
+                    100 + 10 * k + k,
+                    channels=channels,
+                    title='Decoding weights')
+
+
+def plot_filters(filters):
+    '''Create a plot of conv filters, visualized as pixel arrays.'''
+    imgs = filters.get_value()
+
+    N, channels, x, y = imgs.shape
+    n = int(np.sqrt(N))
+    assert n * n == N, 'filters must contain a square number of rows!'
+    assert channels == 1 or channels == 3, 'can only plot grayscale or rgb filters!'
+
+    img = np.zeros(((y+1) * n - 1, (x+1) * n - 1, channels), dtype=imgs[0].dtype)
+    for i, pix in enumerate(imgs):
+        r, c = divmod(i, n)
+        img[r * (y+1):(r+1) * (y+1) - 1,
+            c * (x+1):(c+1) * (x+1) - 1] = pix.transpose((1, 2, 0))
+
+    img -= img.min()
+    img /= img.max()
+
+    ax = plt.gcf().add_subplot(111)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    ax.set_frame_on(False)
+    ax.imshow(img.squeeze(), cmap=plt.cm.gray)
